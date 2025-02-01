99ebd8a7a2d5a46abdfa1a74b40b0daff809d1c1
Benjas333 <seston30@gmail.com>
2025-02-01 03:44:51 -0600

    Fixed not emitting error event when failing to read jar manifest General improvements Minor changes

--- src/Minecraft-Loader/patcher.ts
+++ src/Minecraft-Loader/patcher.ts
@@ -14,82 +14,81 @@ export default class forgePatcher extends EventEmitter {
     }
 
     async patcher(profile: any, config: any, neoForgeOld: boolean = true) {
-        let { processors } = profile;
+        const { processors } = profile;
 
         for (let key in processors) {
-            if (Object.prototype.hasOwnProperty.call(processors, key)) {
-                let processor = processors[key];
-                if (processor?.sides && !(processor?.sides || []).includes('client')) {
-                    continue;
-                }
+            if (!Object.prototype.hasOwnProperty.call(processors, key)) continue;
 
-                let jar = getPathLibraries(processor.jar)
-                let filePath = path.resolve(this.options.path, 'libraries', jar.path, jar.name)
+            const processor = processors[key];
+            if (processor?.sides && !(processor?.sides || []).includes('client')) continue;
 
-                let args = processor.args.map(arg => this.setArgument(arg, profile, config, neoForgeOld)).map(arg => this.computePath(arg));
-                let classPaths = processor.classpath.map(cp => {
-                    let classPath = getPathLibraries(cp)
-                    return `"${path.join(this.options.path, 'libraries', `${classPath.path}/${classPath.name}`)}"`
-                });
-                let mainClass = await this.readJarManifest(filePath);
+            const jar = getPathLibraries(processor.jar);
+            const filePath = path.resolve(this.options.path, 'libraries', jar.path, jar.name);
 
-                await new Promise((resolve: any) => {
-                    const ps = spawn(
-                        `"${path.resolve(config.java)}"`,
-                        [
-                            '-classpath',
-                            [`"${filePath}"`, ...classPaths].join(path.delimiter),
-                            mainClass,
-                            ...args
-                        ], { shell: true }
-                    );
-
-                    ps.stdout.on('data', data => {
-                        this.emit('patch', data.toString('utf-8'))
-                    });
-
-                    ps.stderr.on('data', data => {
-                        this.emit('patch', data.toString('utf-8'))
-                    });
-
-                    ps.on('close', code => {
-                        if (code !== 0) {
-                            this.emit('error', `Forge patcher exited with code ${code}`);
-                            resolve();
-                        }
-                        resolve();
-                    });
-                });
+            const args = processor.args.map(arg => this.setArgument(arg, profile, config, neoForgeOld)).map(arg => this.computePath(arg));
+            const classPaths = processor.classpath.map(cp => {
+                const classPath = getPathLibraries(cp);
+                return `"${path.join(this.options.path, 'libraries', `${classPath.path}/${classPath.name}`)}"`;
+            });
+            let mainClass;
+            try {
+                mainClass = await this.readJarManifest(filePath);
+            } catch (error) {
+                this.emit('error', `Failed to read jar manifest: ${error.message}`);
             }
+
+            await new Promise((resolve: any) => {
+                const ps = spawn(
+                    `"${path.resolve(config.java)}"`,
+                    [
+                        '-classpath',
+                        [`"${filePath}"`, ...classPaths].join(path.delimiter),
+                        mainClass,
+                        ...args
+                    ], { shell: true }
+                );
+
+                ps.stdout.on('data', data => {
+                    this.emit('patch', data.toString('utf-8'));
+                });
+
+                ps.stderr.on('data', data => {
+                    this.emit('patch', data.toString('utf-8'));
+                });
+
+                ps.on('close', code => {
+                    if (code === 0) resolve();
+                    this.emit('error', `Forge patcher exited with code ${code}`);
+                    resolve();
+                });
+            });
         }
 
     }
 
     check(profile: any) {
         let files = [];
-        let { processors } = profile;
+        const { processors } = profile;
 
         for (let key in processors) {
-            if (Object.prototype.hasOwnProperty.call(processors, key)) {
-                let processor = processors[key];
-                if (processor?.sides && !(processor?.sides || []).includes('client')) continue;
+            if (!Object.prototype.hasOwnProperty.call(processors, key)) continue;
 
-                processor.args.map(arg => {
-                    let finalArg = arg.replace('{', '').replace('}', '');
-                    if (profile.data[finalArg]) {
-                        if (finalArg === 'BINPATCH') return
-                        files.push(profile.data[finalArg].client)
-                    }
-                })
-            }
+            let processor = processors[key];
+            if (processor?.sides && !(processor?.sides || []).includes('client')) continue;
+
+            processor.args.map(arg => {
+                let finalArg = arg.replace('{', '').replace('}', '');
+                if (!profile.data[finalArg] || finalArg === 'BINPATCH') return;
+                files.push(profile.data[finalArg].client);
+            });
         }
 
         files = files.filter((item, index) => files.indexOf(item) === index);
 
         for (let file of files) {
-            let libMCP = getPathLibraries(file.replace('[', '').replace(']', ''))
+            let libMCP = getPathLibraries(file.replace('[', '').replace(']', ''));
             file = `${path.resolve(this.options.path, 'libraries', `${libMCP.path}/${libMCP.name}`)}`;
-            if (!fs.existsSync(file)) return false
+            if (!fs.existsSync(file)) return false;
         }
         return true;
     }
@@ -102,13 +101,11 @@ export default class forgePatcher extends EventEmitter {
         })
 
         if (profile.data[finalArg]) {
-            if (finalArg === 'BINPATCH') {
-                let clientdata = getPathLibraries(profile.path || universalPath.name)
-                return `"${path
-                    .join(this.options.path, 'libraries', `${clientdata.path}/${clientdata.name}`)
-                    .replace('.jar', '-clientdata.lzma')}"`;
-            }
-            return profile.data[finalArg].client;
+            if (finalArg !== 'BINPATCH') return profile.data[finalArg].client;
+            let clientdata = getPathLibraries(profile.path || universalPath.name)
+            return `"${path
+                .join(this.options.path, 'libraries', `${clientdata.path}/${clientdata.name}`)
+                .replace('.jar', '-clientdata.lzma')}"`;
         }
 
         return arg
@@ -121,17 +118,16 @@ export default class forgePatcher extends EventEmitter {
     }
 
     computePath(arg: any) {
-        if (arg[0] === '[') {
-            let libMCP = getPathLibraries(arg.replace('[', '').replace(']', ''))
-            return `"${path.join(this.options.path, 'libraries', `${libMCP.path}/${libMCP.name}`)}"`;
-        }
-        return arg;
+        if (arg[0] !== '[') return arg;
+
+        let libMCP = getPathLibraries(arg.replace('[', '').replace(']', ''))
+        return `"${path.join(this.options.path, 'libraries', `${libMCP.path}/${libMCP.name}`)}"`;
     }
 
     async readJarManifest(jarPath: string) {
         let extraction: any = await getFileFromArchive(jarPath, 'META-INF/MANIFEST.MF');
 
-        if (extraction) return (extraction.toString("utf8")).split('Main-Class: ')[1].split('\r\n')[0];
-        return null;
+        if (!extraction) return null;
+        return (extraction.toString("utf8")).split('Main-Class: ')[1].split('\r\n')[0];
     }
 }
